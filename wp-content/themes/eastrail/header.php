9f01e636197136962187618f96489c1f15c9d1dd
Joe di Stefano <joeydi@okaypl.us>
2023-03-31 11:21:19 -0400

    update favicon

--- wp-content/themes/eastrail/header.php
+++ wp-content/themes/eastrail/header.php
@@ -9,7 +9,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo ET::theme_url('/static/favicon/favicon-32x32.png'); ?>">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo ET::theme_url('/static/favicon/favicon-16x16.png'); ?>">
     <link rel="manifest" href="<?php echo ET::theme_url('/static/favicon/site.webmanifest'); ?>">
-    <link rel="mask-icon" href="<?php echo ET::theme_url('/static/favicon/safari-pinned-tab.svg'); ?>" color="#f99d33">
+    <link rel="mask-icon" href="<?php echo ET::theme_url('/static/favicon/safari-pinned-tab.svg'); ?>" color="#00ae42">
     <link rel="shortcut icon" href="<?php echo ET::theme_url('/static/favicon/favicon.ico'); ?>">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-config" content="<?php echo ET::theme_url('/static/favicon/browserconfig.xml'); ?>">
