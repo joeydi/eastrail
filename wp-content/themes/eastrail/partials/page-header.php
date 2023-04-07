b7bf1b609b98c0b074323ec51f0ba43a7912bef4
Joe di Stefano <joeydi@okaypl.us>
2023-04-07 11:44:14 -0400

    page header updates

--- wp-content/themes/eastrail/partials/page-header.php
+++ wp-content/themes/eastrail/partials/page-header.php
@@ -1,56 +1,39 @@
 <?php if (have_rows('page_header')) : while (have_rows('page_header')) : the_row(); ?>
-        <?php if (get_row_layout() == 'small') : ?>
-            <section class="page-header page-header-small">
-                <div class="container" data-scroll-fade>
-                    <div class="row">
-                        <div class="col-md-10 col-lg-8">
+        <?php if (get_row_layout() == 'basic') : ?>
+            <section class="page-header page-header-basic text-center">
+                <div class="container">
+                    <div class="row justify-content-center" data-scroll-fade-children>
+                        <div class="col-10">
                             <?php $heading = get_sub_field('heading') ?: get_the_title(); ?>
                             <h1><?php echo $heading; ?></h1>
                         </div>
+                        <?php if ($subheading = get_sub_field('subheading')) : ?>
+                            <div class="col-8 mt-30 font-weight-bold">
+                                <?php echo $subheading; ?>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 </div>
             </section>
-        <?php elseif (get_row_layout() == 'medium') : ?>
-            <section class="page-header page-header-medium">
-                <?php if ($image = get_sub_field('image')) : ?>
-                    <picture class="aspect bg-grey-1">
-                        <?php echo wp_get_attachment_image($image, 'landscape'); ?>
-                    </picture>
-                <?php endif; ?>
-                <div class="container" data-scroll-fade>
-                    <div class="row">
-                        <div class="col-md-7 col-lg-6">
-                            <?php $heading = get_sub_field('heading') ?: get_the_title(); ?>
-                            <h1><?php echo $heading; ?></h1>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        <?php elseif (get_row_layout() == 'large') : ?>
-            <section class="page-header page-header-large">
-                <?php $images = get_sub_field('images'); ?>
-                <?php if (count($images) > 1) : ?>
-                    <div class="swiper">
-                        <div class="swiper-wrapper">
-                            <?php foreach ($images as $image) : ?>
-                                <div class="swiper-slide">
-                                    <picture class="aspect-block bg-black">
-                                        <?php echo wp_get_attachment_image($image, 'block'); ?>
-                                    </picture>
+        <?php elseif (get_row_layout() == 'image') : ?>
+            <section class="page-header page-header-image text-center">
+                <div class="picture-wrapper">
+                    <?php if ($image = get_sub_field('image')) : ?>
+                        <picture class="aspect bg-grey-1">
+                            <?php echo wp_get_attachment_image($image, 'landscape'); ?>
+                        </picture>
+                    <?php endif; ?>
+                    <div class="container">
+                        <div class="row justify-content-center" data-scroll-fade-children>
+                            <div class="col-10">
+                                <?php $heading = get_sub_field('heading') ?: get_the_title(); ?>
+                                <h1><?php echo $heading; ?></h1>
+                            </div>
+                            <?php if ($subheading = get_sub_field('subheading')) : ?>
+                                <div class="col-8 mt-30 font-weight-bold">
+                                    <?php echo $subheading; ?>
                                 </div>
-                            <?php endforeach; ?>
-                        </div>
-                    </div>
-                <?php else : ?>
-                    <picture class="aspect-block bg-black">
-                        <?php echo wp_get_attachment_image($image, 'block'); ?>
-                    </picture>
-                <?php endif; ?>
-                <div class="container" data-scroll-fade>
-                    <div class="row">
-                        <div class="col-md-7 col-lg-6">
-                            <?php $heading = get_sub_field('heading') ?: get_the_title(); ?>
-                            <h1><?php echo $heading; ?></h1>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
@@ -58,11 +41,10 @@
         <?php endif; ?>
     <?php endwhile; ?>
 <?php else : ?>
-    <section class="page-header page-header-small">
-        <div class="container" data-scroll-fade>
-            <div class="row">
-                <div class="col-md-10 col-lg-8">
-                    <?php $heading = get_sub_field('heading') ?: get_the_title(); ?>
+    <section class="page-header page-header-basic text-center">
+        <div class="container">
+            <div class="row justify-content-center" data-scroll-fade-children>
+                <div class="col-10">
                     <h1><?php the_title(); ?></h1>
                 </div>
             </div>
