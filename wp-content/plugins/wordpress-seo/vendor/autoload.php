07feb3014699f3ab0202eaf0ff49d05c2ae1910b
Joe di Stefano <joeydi@okaypl.us>
2023-05-05 14:33:34 -0400

    update all plugins

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5f169a3608cd329ef13ae362fe2c82a7::getLoader();
+return ComposerAutoloaderInitf2a10bb3b36cfb2d18ddbc18eedc2686::getLoader();
