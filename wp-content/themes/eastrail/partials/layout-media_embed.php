01ba701bd228d0bf6a0e8ce86ca4a908c1146fd6
Joe di Stefano <joeydi@okaypl.us>
2025-10-14 12:48:26 -0400

    use ET::video_embed in media embed template

--- wp-content/themes/eastrail/partials/layout-media_embed.php
+++ wp-content/themes/eastrail/partials/layout-media_embed.php
@@ -7,17 +7,17 @@ $justify_content = get_sub_field('justify_content');
 
 ?>
 <section class="media-embed <?php echo ET::section_classes(); ?>" id="<?php echo sanitize_title($label); ?>" aria-label="<?php echo $label; ?>">
-    <div class="container">
-        <?php if ($content) : ?>
-            <div class="row mb-40 mb-lg-60 <?php echo $justify_content; ?>">
-                <div class="col-lg-10 col-xl-8" data-scroll-fade-children>
-                    <?php echo $content; ?>
-                </div>
-            </div>
-        <?php endif; ?>
-
-        <div data-scroll-fade>
-            <?php echo wp_oembed_get($media_url); ?>
+  <div class="container">
+    <?php if ($content) : ?>
+      <div class="row mb-40 mb-lg-60 <?php echo $justify_content; ?>">
+        <div class="col-lg-10 col-xl-8" data-scroll-fade-children>
+          <?php echo $content; ?>
         </div>
+      </div>
+    <?php endif; ?>
+
+    <div data-scroll-fade>
+      <?php echo ET::video_embed($media_url); ?>
     </div>
+  </div>
 </section>
